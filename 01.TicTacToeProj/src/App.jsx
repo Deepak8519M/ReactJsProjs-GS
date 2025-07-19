ff23a40ef016c9e564ac1e1475beffd20e315895
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-19 16:43:24 +0530

    folder path changed

--- 01.TicTacToeProj/src/App.jsx
+++ 01.TicTacToeProj/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TicTacToe from "./components/TicTacToe/TicTacToe";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
 }
 
 export default App;
--- 01.TicTacToeProj/src/components/TicTacToe/TicTacToe.jsx
+++ 01.TicTacToeProj/src/components/TicTacToe/TicTacToe.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./TicTacToe.css";
 
 function TicTacToe() {
   return (
