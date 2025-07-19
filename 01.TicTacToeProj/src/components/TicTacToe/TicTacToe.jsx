7a41a6f79bb9a64921d5218bdc9be1c557cedbac
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-19 16:44:53 +0530

    assets imported

--- 01.TicTacToeProj/src/components/TicTacToe/TicTacToe.jsx
+++ 01.TicTacToeProj/src/components/TicTacToe/TicTacToe.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./TicTacToe.css";
 
+import circle_icon from "../Assets/circle.png";
+import cross_icon from "../Assets/cross.png";
+
 function TicTacToe() {
   return (
     <div>
