7433bcddd69bf34db2e484747beddfc5a61bf4c4
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-20 19:20:51 +0530

    structure of quiz app added

--- 02.QuizApp/src/App.jsx
+++ 02.QuizApp/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Quiz from "./components/Quiz/Quiz";
 
 function App() {
   return (
-    <div>
-      <h1>Quiz App</h1>
-    </div>
+    <>
+      <Quiz />
+    </>
   );
 }
 
--- 02.QuizApp/src/components/Quiz/Quiz.jsx
+++ 02.QuizApp/src/components/Quiz/Quiz.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Quiz.css";
 
 function Quiz() {
   return (
-    <div>
-      
+    <div className="container">
+      <h1>Quiz App</h1>
+      <hr />
+      <h2>1.Which device is required to Internet Connection</h2>
+      <ul>
+        <li>Modem</li>
+        <li>Router</li>
+        <li>LAN Cable</li>
+        <li>Pen Drive</li>
+      </ul>
+      <button>Next</button>
+      <div className="index">1 of 5 questions</div>
     </div>
-  )
+  );
 }
 
-export default Quiz
+export default Quiz;
