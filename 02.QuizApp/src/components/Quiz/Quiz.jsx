22adfd1437079b3a73bc8dae29c265c5f1841030
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-20 19:42:30 +0530

    check ans logic and style added

--- 02.QuizApp/src/components/Quiz/Quiz.jsx
+++ 02.QuizApp/src/components/Quiz/Quiz.jsx
@@ -10,7 +10,7 @@ function Quiz() {
     if (question.ans === ans) {
       e.target.classList.add("correct");
     } else {
-      e.target.classList.add("correct");
+      e.target.classList.add("wrong");
     }
   };
 
@@ -22,10 +22,34 @@ function Quiz() {
         {index + 1}.{question.question}
       </h2>
       <ul>
-        <li>{question.option1}</li>
-        <li>{question.option2}</li>
-        <li>{question.option3}</li>
-        <li>{question.option4}</li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 1);
+          }}
+        >
+          {question.option1}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 2);
+          }}
+        >
+          {question.option2}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 3);
+          }}
+        >
+          {question.option3}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 4);
+          }}
+        >
+          {question.option4}
+        </li>
       </ul>
       <button>Next</button>
       <div className="index">1 of 5 questions</div>
